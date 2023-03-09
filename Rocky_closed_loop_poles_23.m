8dccfab9830e64ea42d4d4265227c7e90af7e4a7
kclamoreux <karina.lamoreux@gmail.com>
2023-03-09 10:52:59 -0500

    Recalibration for rocky 3

--- Rocky_closed_loop_poles_23.m
+++ Rocky_closed_loop_poles_23.m
@@ -47,9 +47,9 @@ Hcloop_sub = subs(Hcloop) % sub parameter values into Hcloop
 % specify locations of the target poles,
 % choose # based on order of Htot denominator
 % e.g., want some oscillations, want fast decay, etc. 
-p1 = -1 + 2*pi*i    % dominant pole pair
-p2 = -1 - 2*pi*i    % dominant pole pair 
-p3 = -10
+p1 = -5 + 0.25*2*pi*i    % dominant pole pair
+p2 = -5 - 0.25*2*pi*i    % dominant pole pair 
+p3 = -100
 
 
 % target characteristic polynomial
